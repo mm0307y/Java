257f6a45d48b5bb148da63a9428957313d6e0f72
DESKTOP-0FTU5EG\KOSMO <mm0307a@gmail.com>
2024-11-18 20:19:10 +0900

    2024년 11월 18일 수업

---
+++ workspace_java/basic1/src/inheritance_11_18/step3_11_18/VectorSimulation_11_18.java
@@ -0,0 +1,9 @@
+package inheritance_11_18.step3_11_18;
+
+import java.util.Vector;
+
+public class VectorSimulation_11_18 {
+    public static void main(String[] args) {
+        Vector vector = new Vector();
+    }
+}
