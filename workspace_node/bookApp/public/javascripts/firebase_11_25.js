ab46e0263c9ddfcb61489b6867c790bacab6da6a
DESKTOP-0FTU5EG\KOSMO <mm0307a@gmail.com>
2024-11-25 19:01:16 +0900

    2024년 11월 25일 수업

---
+++ workspace_node/bookApp/public/javascripts/firebase_11_25.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAF_QeapaYa0SO4Q4RLivzjodBExQ8HsZQ",
+    authDomain: "slalom2024-d0918.firebaseapp.com",
+    projectId: "slalom2024-d0918",
+    storageBucket: "slalom2024-d0918.firebasestorage.app",
+    messagingSenderId: "1014655866177",
+    appId: "1:1014655866177:web:a99f6ce99ce1f4c6943dc0"
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- workspace_node/bookApp/routes/users.js
+++ workspace_node/bookApp/routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
+/* 회원가입 추가 */
+router.get('/join', function(req, res, next) {
+    res.render('index', {title: "회원가입", pageName : "users_11_25/join_11_25.ejs"});
+})
+
+/* 회원가입 추가 */
 module.exports = router;
