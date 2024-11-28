6a47db987eacaad070ce9356b00c259fffd9ab6e
DESKTOP-0FTU5EG\KOSMO <mm0307a@gmail.com>
2024-11-28 18:11:09 +0900

    2024년 11월 28일 수업

--- workspace_node/bookApp - kakao/public/javascripts/firebase_11_25.js
+++ workspace_node/bookApp - kakao/public/javascripts/firebase_11_25.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+// { initializeApp } - 구조분해 할당
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 // 구글 파이어베이스에서 제공하는 firebase-app.jsp에 intializeApp함수가 정의되어 있다.
 // 외부에 js파일에 제공하는 함수를 사용하려면 import를 할 수 있다. - NodeJs - 브라우저와 별개로 작업이 가능하다.
@@ -20,6 +21,10 @@ const firebaseConfig = {
 // header.ejs에서 사용하기 위해서 변수 선언 앞에 export를 붙인다.
 // 로그인(구글 이메일 비번)을 진행하는 login.ejs에서도 app객체를 사용한다.
 // 해당 파일(login.ejs, header.ejs)에서 app출력해 보면 위 firebaseConfig에 내용이 있다.
+// initializeApp 함수는 firebase-app.js에 구현되어 있다.
+// 함수에는 파라미터를 가질 수 있다. 그런데 파라미터 갯수가 6개이니까 하나로 넘긴다.({} -> object) 좌, 우괄호가 있어야한다.
+// object 키와 값으로 구성 됐다.
+// npm(웹 레포지토리)
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
