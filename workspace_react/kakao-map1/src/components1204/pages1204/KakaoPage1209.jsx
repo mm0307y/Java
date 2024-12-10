ff7da87038901d0010aa4ecbdaf2c991709ecf95
mm0307y <mm0307a@gmail.com>
2024-12-10 15:31:53 +0900

    2024년 12월 10일 수업

--- workspace_react/kakao-map1/src/components1204/pages1204/KakaoPage1209.jsx
+++ workspace_react/kakao-map1/src/components1204/pages1204/KakaoPage1209.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header1204 from '../include1204/Header1204'
 import Footer1204 from '../include1204/Footer1204'
-import KakaoMap1209 from '../kakao1209/KakaoMap1209'
+import MultMarker1210 from '../kakao1209/MultMarker1210'
+// import KakaoMap1209 from '../kakao1209/KakaoMap1209'
 
 const KakaoPage1209 = () => {
   return (
     <>
       <Header1204 />
-      <KakaoMap1209 />
+      {/* <KakaoMap1209 /> */}
+      <MultMarker1210 />
       <Footer1204 />
     </>
   )
