33fae2319f70a1c0dfb42f96dbeddb060484b1b0
mm0307y <mm0307a@gmail.com>
2024-12-10 18:37:59 +0900

    2024년 12월 10일 수업

--- workspace_react/cloudinary-step1/src/index.js
+++ workspace_react/cloudinary-step1/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BlogApp from './BlogApp';
+import CardApp1210 from './CardApp1210';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <>
-    <BlogApp />
+    <CardApp1210 />
   </>
 );
