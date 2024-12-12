ee4fc57b817c8b05201896ef92cfc90a7f36b9d5
mm0307y <mm0307a@gmail.com>
2024-12-12 12:17:49 +0900

    2024년 12월 12일 3교시 수업2

---
+++ workspace_react/cloudinary-step2/src/service1212/firebase1212.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAF_QeapaYa0SO4Q4RLivzjodBExQ8HsZQ",
+  authDomain: "slalom2024-d0918.firebaseapp.com",
+  databaseURL: "https://slalom2024-d0918-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "slalom2024-d0918",
+  storageBucket: "slalom2024-d0918.firebasestorage.app",
+  messagingSenderId: "1014655866177",
+  appId: "1:1014655866177:web:a99f6ce99ce1f4c6943dc0"
+};
+
+export const app = initializeApp(firebaseConfig);
